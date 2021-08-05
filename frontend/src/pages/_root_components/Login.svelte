c484e0368abf1bf271641dd9702d2faf7a15e8a2
SMEETT <tbr@tutamail.com>
2021-08-05 16:15:34 +0200

    reworked Login-Form (fixes #13)

--- frontend/src/pages/_root_components/Login.svelte
+++ frontend/src/pages/_root_components/Login.svelte
@@ -21,11 +21,29 @@
 
 <!-- Login Index -->
 
-<h1>Login</h1>
-<form form on:submit|preventDefault={handleSubmit}>
+<div class="headline">
+	<h1 class="color-headline" style="margin-left: 0rem">Login</h1>
+</div>
+<div style="margin-top: -2rem" class="separator" />
+
+<form form>
 	<label for="email">E-Mail:</label><br />
 	<input type="text" id="email" name="email" bind:value={email} /><br /><br />
 	<label for="password">Password:</label><br />
-	<input type="text" id="password" name="password" bind:value={password} /><br /><br />
-	<input id="btnSubmit" type="submit" value="Submit" />
+	<input type="password" id="password" name="password" bind:value={password} /><br /><br />
 </form>
+<button on:click={handleSubmit} class="btn btn-regular">Absenden</button>
+
+<style>
+	input {
+		border: 1px solid var(--dark);
+		border-radius: 5px;
+		padding-left: 1rem;
+		margin-top: 0.5rem;
+	}
+
+	button {
+		margin-top: 2rem;
+		width: 100%;
+	}
+</style>
