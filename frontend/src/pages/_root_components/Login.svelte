7d7f468589baeb36267750990254d9ad2c1015d7
SMEETT <tbr@tutamail.com>
2021-08-06 13:18:57 +0200

    Added Login-Form Frontend-Validation (fixes #23)

--- frontend/src/pages/_root_components/Login.svelte
+++ frontend/src/pages/_root_components/Login.svelte
@@ -1,22 +1,82 @@
 <!-- routify:options reset -->
 <script>
 	import { metatags, goto } from "@roxi/routify";
+	import { null_to_empty } from "svelte/internal";
 	metatags.title = "Dogwatch / Login";
 	metatags.description = "Description coming soon...";
-	import { isAuthenticated, authenticating, checkAuthCookie, statusModalMessages } from "../../stores/state";
+	import { isAuthenticated, authenticating, checkAuthCookie, liveValidation, statusModalMessages } from "../../stores/state";
 
 	import { login } from "../../stores/state";
+	import * as yup from "yup";
 
 	let email;
 	let password;
 
+	const loginData = {
+		email: null,
+		password: null,
+	};
+
 	async function handleSubmit() {
-		if (await login(email, password)) {
-			$goto("/appointments");
-			statusModalMessages.set({ code: 200, message: "Login erfolgreich" });
-		} else {
-			console.log("login failed");
-			$statusModalMessages = { code: 400, message: "Login fehlgeschlagen" };
+		if (await validate(schema_login, loginData, loginValidationErrors)) {
+			if (await login(loginData.email, loginData.password)) {
+				$goto("/appointments");
+				statusModalMessages.set({ code: 200, message: "Login erfolgreich" });
+			} else {
+				console.log("login failed");
+				$statusModalMessages = { code: 400, message: "Falsche E-Mail Adresse/Passwort" };
+			}
+		}
+	}
+
+	// ********************************************************
+	// VALIDATION
+	// ********************************************************
+
+	// errors Frontend-Validation
+	let loginValidationErrors = {};
+
+	// ----------------------------------------------------
+	// NEW APPOINTMENT SCHEMA
+	// ----------------------------------------------------
+	const schema_login = yup.object().shape({
+		email: yup.string().email("Ungueltige E-Mail Adresse").required("Bitte E-Mail angeben").typeError("Bitte E-Mail angeben"),
+		password: yup.string().required("Passwort bitte angeben").typeError("Bitte Passwort angeben"),
+	});
+
+	// ----------------------------------------------------
+	// VALIDATION
+	// ----------------------------------------------------
+	const validate = async (schema, data, errors) => {
+		try {
+			await schema.validate(data, { abortEarly: false });
+			// alert(JSON.stringify(get(data), null, 2));
+			errors = {};
+			loginValidationErrors = {};
+			return true;
+		} catch (err) {
+			console.log(errors);
+			errors = extractErrors(err);
+			console.log(errors);
+			loginValidationErrors = errors;
+			$statusModalMessages = { code: 1, message: "Bitte die fehlenden Felder ausfuellen" };
+			return false;
+		}
+	};
+
+	// ----------------------------------------------------
+	// EXTRACT VALIDATION ERRORS (FRONTEND)
+	// ----------------------------------------------------
+	function extractErrors(err) {
+		return err.inner.reduce((acc, err) => {
+			return { ...acc, [err.path]: err.message };
+		}, {});
+	}
+
+	$: validateLive(loginData);
+	function validateLive() {
+		if ($liveValidation) {
+			validate(schema_login, loginData, loginValidationErrors);
 		}
 	}
 </script>
@@ -30,11 +90,17 @@
 
 <form form>
 	<label for="email">E-Mail:</label><br />
-	<input type="text" id="email" name="email" bind:value={email} /><br /><br />
+	<input type="text" id="email" name="email" bind:value={loginData.email} />
+	{#if loginValidationErrors.email}
+		<p class="form-validation-error" style="margin-bottom: 2rem">({loginValidationErrors.email})</p>
+	{/if}
 	<label for="password">Password:</label><br />
-	<input type="password" id="password" name="password" bind:value={password} /><br /><br />
+	<input type="password" id="password" name="password" bind:value={loginData.password} />
+	{#if loginValidationErrors.password}
+		<p class="form-validation-error">({loginValidationErrors.password})</p>
+	{/if}
 </form>
-<button on:click={handleSubmit} class="btn btn-regular">Absenden</button>
+<button on:click={handleSubmit} class="btn btn-regular">Einloggen</button>
 
 <style>
 	input {
@@ -42,6 +108,7 @@
 		border-radius: 5px;
 		padding-left: 1rem;
 		margin-top: 0.5rem;
+		margin-bottom: 1rem;
 	}
 
 	button {
